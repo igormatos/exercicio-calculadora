7a5ee604886478c72dd66a334e8ce4169bfef1db
Igor Matos <igor.matos@liferay.com>
2018-09-07 13:24:36 -0300

    exercicio

--- calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
+++ calculadora/app/src/main/java/br/ufpe/cin/if710/calculadora/MainActivity.kt
@@ -2,12 +2,75 @@ package br.ufpe.cin.if710.calculadora
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
+
+    companion object {
+
+        const val EXTRA_TEXT_INFO = "EXTRA_TEXT_INFO"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Passo 4
+        savedInstanceState?.let {
+            text_info.setText(it.getString(EXTRA_TEXT_INFO))
+        }
+
+        // Passo 1
+        btn_0.setOnClickListener { clickOnButton(it as Button) }
+        btn_1.setOnClickListener { clickOnButton(it as Button) }
+        btn_2.setOnClickListener { clickOnButton(it as Button) }
+        btn_3.setOnClickListener { clickOnButton(it as Button) }
+        btn_4.setOnClickListener { clickOnButton(it as Button) }
+        btn_5.setOnClickListener { clickOnButton(it as Button) }
+        btn_6.setOnClickListener { clickOnButton(it as Button) }
+        btn_7.setOnClickListener { clickOnButton(it as Button) }
+        btn_8.setOnClickListener { clickOnButton(it as Button) }
+        btn_9.setOnClickListener { clickOnButton(it as Button) }
+        btn_Add.setOnClickListener { clickOnButton(it as Button) }
+        btn_Divide.setOnClickListener { clickOnButton(it as Button) }
+        btn_Dot.setOnClickListener { clickOnButton(it as Button) }
+        btn_Multiply.setOnClickListener { clickOnButton(it as Button) }
+        btn_Subtract.setOnClickListener { clickOnButton(it as Button) }
+        btn_LParen.setOnClickListener { clickOnButton(it as Button) }
+        btn_RParen.setOnClickListener { clickOnButton(it as Button) }
+        btn_Power.setOnClickListener { clickOnButton(it as Button) }
+
+        btn_Clear.setOnClickListener { text_calc.text.clear() }
+
+        // Passo 2
+        btn_Equal.setOnClickListener {
+
+            val expression = text_calc.text.toString()
+
+            // Passo 3 - try catch para tratarmos a exceção, disparando um Toast
+            try {
+                val result = eval(expression).toString()
+                text_info.text = result
+                text_calc.setText(result)
+
+            } catch (e: Exception) {
+                invalidExpression()
+            }
+        }
+    }
+
+    // Criei uma função inline para ser usada nos botões que adicionam um simbolo ao edittext
+    val clickOnButton: (Button) -> Unit = {
+        text_calc.setText(text_calc.text.toString() + it.text)
+    }
+
+
+    // Passo 4
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putString(EXTRA_TEXT_INFO, text_info.text.toString())
+        super.onSaveInstanceState(outState)
     }
 
     //Como usar a função:
@@ -93,7 +156,7 @@ class MainActivity : Activity() {
                     else if (func == "tan")
                         x = Math.tan(Math.toRadians(x))
                     else
-                        throw RuntimeException("Função desconhecida: " + func)
+                        throw RuntimeException("Função invalida")
                 } else {
                     throw RuntimeException("Caractere inesperado: " + ch.toChar())
                 }
@@ -101,5 +164,10 @@ class MainActivity : Activity() {
                 return x
             }
         }.parse()
+    }
+
+    fun invalidExpression() {
+        Toast.makeText(applicationContext, "Função desconhecida", Toast.LENGTH_SHORT).show()
+        text_calc.text.clear()
     }
 }
